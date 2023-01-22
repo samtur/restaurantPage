bc31b621728e1e1886eb42e8ae4406d74d5015d4
samtur <samturland23@gmail.com>
2023-01-22 00:03:28 +0000

    Added font and background images.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 import "./style.css";
+import BackgroundImg from "./background.jpg";
+
+function component() {
+  const content = document.querySelector(".content");
+
+  // Add the image to our existing div.
+  const BackgroundImg = new Image();
+
+  content.appendChild(BackgroundImg);
+  return element;
+}
+
+document.body.appendChild(component());
