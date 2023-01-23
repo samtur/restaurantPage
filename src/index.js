51784392c185e5ca5e5e69cd71d901470cde860b
samtur <samturland23@gmail.com>
2023-01-23 00:06:49 +0000

    Styled CSS

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import "./style.css";
-import BackgroundImg from "./background.jpg";
+// import BackgroundImg from "./background.jpg";
 
-function component() {
-  const content = document.querySelector(".content");
+// function component() {
+//   const content = document.querySelector(".content");
 
-  // Add the image to our existing div.
-  const BackgroundImg = new Image();
+//   // Add the image to our existing div.
+//   const BackgroundImg = new Image();
 
-  content.appendChild(BackgroundImg);
-  return element;
-}
+//   content.appendChild(BackgroundImg);
+//   return element;
+// }
 
-document.body.appendChild(component());
+// document.body.appendChild(component());
