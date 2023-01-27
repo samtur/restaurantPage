0bceb14a61d7ea8ca361446ebd0bacd04e5ec4cd
samtur <samturland23@gmail.com>
2023-01-27 21:24:17 +0000

    Added logic to cycle modules

--- src/contact.js
+++ src/contact.js
@@ -1,4 +1,5 @@
-const menuFunction = () => {
+const contactFunction = () => {
+  const content = document.querySelector("#content");
   // Title Card
   const titleCard = document.createElement("div");
   titleCard.classList.add("titleCard");
@@ -27,5 +28,30 @@ const menuFunction = () => {
   linksContainer.appendChild(contact);
 
   // Contact Card
-  
+  const contactContainer = document.createElement("div");
+  contactContainer.classList.add("contactContainer");
+  content.appendChild(contactContainer);
+  const contactCard = document.createElement("div");
+  contactCard.classList.add("contactCard");
+  contactContainer.appendChild(contactCard);
+  const contactInfo = document.createElement("div");
+  contactInfo.classList.add("contactInfo");
+  contactCard.appendChild(contactInfo);
+  const contactImg = document.createElement("div");
+  contactImg.classList.add("contactImg");
+  contactCard.appendChild(contactImg);
+  const contactTxt1 = document.createElement("p");
+  const contactTxt2 = document.createElement("p");
+  const contactTxt3 = document.createElement("p");
+  contactTxt1.innerHTML = "&#x2709; machiavellis@gmail.co.it";
+  contactTxt2.innerHTML = "&#x1F4DE; 123-666-199";
+  contactTxt3.innerHTML = "&#x1F4CD; Viale Glorioso, 18, 00153 Roma RM, Italy";
+  contactTxt1.classList.add("contacttxt");
+  contactTxt2.classList.add("contacttxt");
+  contactTxt3.classList.add("contacttxt");
+  contactInfo.appendChild(contactTxt1);
+  contactInfo.appendChild(contactTxt2);
+  contactInfo.appendChild(contactTxt3);
 };
+
+export { contactFunction };
--- src/index.js
+++ src/index.js
@@ -1,4 +1,53 @@
 import "./style.css";
 import { homeFunction } from "./home";
+import { menuFunction } from "./menu";
+import { contactFunction } from "./contact";
 
-// homeFunction();
+const clear = () => {
+  const content = document.querySelector("#content");
+  let child = content.lastElementChild;
+  while (child) {
+    content.removeChild(child);
+    child = content.lastElementChild;
+  }
+};
+
+const homePage = () => {
+  clear();
+  homeFunction();
+  const homebtn = document.querySelector(".home");
+  const menubtn = document.querySelector(".menu");
+  const contactbtn = document.querySelector(".contact");
+  homebtn.addEventListener("click", homePage);
+  menubtn.addEventListener("click", menuPage);
+  contactbtn.addEventListener("click", contactPage);
+};
+const menuPage = () => {
+  clear();
+  menuFunction();
+  const homebtn = document.querySelector(".home");
+  const menubtn = document.querySelector(".menu");
+  const contactbtn = document.querySelector(".contact");
+  homebtn.addEventListener("click", homePage);
+  menubtn.addEventListener("click", menuPage);
+  contactbtn.addEventListener("click", contactPage);
+};
+const contactPage = () => {
+  clear();
+  contactFunction();
+  const homebtn = document.querySelector(".home");
+  const menubtn = document.querySelector(".menu");
+  const contactbtn = document.querySelector(".contact");
+  homebtn.addEventListener("click", homePage);
+  menubtn.addEventListener("click", menuPage);
+  contactbtn.addEventListener("click", contactPage);
+};
+
+homeFunction();
+
+const homebtn = document.querySelector(".home");
+const menubtn = document.querySelector(".menu");
+const contactbtn = document.querySelector(".contact");
+homebtn.addEventListener("click", homePage);
+menubtn.addEventListener("click", menuPage);
+contactbtn.addEventListener("click", contactPage);
